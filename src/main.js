914964d3d2943612b80fc75d6de030f41af0a2e1
spp90s <shipeng1990s@gmail.com>
2018-11-24 17:05:38 +0800

    项目头尾结构搭建

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
+// main.js是项目入口，所有全局的东西都写到main.js里
+
 import Vue from 'vue'
 import App from './App.vue'
+
+// 导入样式（在App.vue里也可以导入）
+// import './assets/statics/site/css/style.css';
 
 // Vue.config.productionTip = false
 
