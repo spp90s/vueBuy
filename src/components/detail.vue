ce5d41d2ff5053f7dfbb08cad9989ca4d16da8c0
spp90s <shipeng1990s@gmail.com>
2020-12-31 12:13:59 +0800

    随便改改

--- src/components/detail.vue
+++ src/components/detail.vue
@@ -5,7 +5,7 @@
     <div class="section">
       <div class="location">
         <span>当前位置：</span>
-        <a href="/index.html">首页</a> &gt;
+        <a href="/index.html">首页!!!</a> &gt;
         <a href="/goods.html">购物商城</a> &gt;
         <a href="/goods/42/1.html">商品详情</a>
       </div>
